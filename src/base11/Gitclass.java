cc5142ef05b5e211fc81ef5338523e671bff27e9
sravani142017 <darapureddygirija@gmail.com>
2019-06-13 12:24:41 +0530

    basse class created

---
+++ src/base11/Gitclass.java
@@ -0,0 +1,11 @@
+package base11;
+
+public class Gitclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("base class created");
+
+	}
+
+}
