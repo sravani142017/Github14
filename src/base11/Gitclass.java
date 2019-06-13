12713d78baf9701dc18129e4a17eb510dc37be07
sravani142017 <darapureddygirija@gmail.com>
2019-06-13 12:51:12 +0530

    Program modified

--- src/base11/Gitclass.java
+++ src/base11/Gitclass.java
@@ -5,7 +5,7 @@ public class Gitclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("base class created");
-
+System.out.println("added program");
 	}
 
 }
